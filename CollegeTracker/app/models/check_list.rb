6baca280ae0c8bd21370a124140e25d366a5314b
Lauren Grant <dangerlauren@gmail.com>
2015-09-28 16:35:04 -0500

    more model stuff

--- CollegeTracker/app/models/check_list.rb
+++ CollegeTracker/app/models/check_list.rb
@@ -1,5 +1,4 @@
 class CheckList < ActiveRecord::Base
 	belongs_to :college
 	belongs_to :user
-	has_many :rec_letters
 end
--- CollegeTracker/app/models/rec_letter.rb
+++ CollegeTracker/app/models/rec_letter.rb
@@ -1,4 +1,3 @@
 class RecLetter < ActiveRecord::Base
-	belongs_to :user
-	has_many :check_lists
+	
 end
