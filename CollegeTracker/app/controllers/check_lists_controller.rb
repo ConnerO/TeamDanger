b67025e6755e8152cf3fc3a613523f48e0017127
Robert Weilbaecher <robertweilbaecher@hotmail.com>
2015-09-28 23:07:32 -0500

    Fixed check_list controller to require login to access, made sure database variables were available for read functions for views

--- CollegeTracker/app/controllers/check_lists_controller.rb
+++ CollegeTracker/app/controllers/check_lists_controller.rb
@@ -1,48 +1,63 @@
 class CheckListsController < ApplicationController
-before_action :check_list_params, :only =>[:create,:update]
 
-def index
-	@check_lists = Check_list.all
-end
+before_filter :require_login
 
-def show
- 	@check_list = Check_list.find(params[:id])
-end
-
-def new
-	@check_list = Check_list.new
-end
-
-def create
-	@check_list = Check_list.new(check_list_params)
-
-	if @check_list.save
-		redirect_to @check_list
+	def index
+		@check_lists = Check_list.all
+		@colleges = College.all
+		current_user
 	end
-end
 
-def edit
-	@check_list = Check_list.find(params[:id])
-end
-
-def update
-	@check_list = Check_list.find(params[:id])
-
-	if @check_list.update(check_list_params)
-		redirect_to @check_list
+	def show
+	 	@check_list = Check_list.find(params[:id])
+	 	@colleges = College.all
+	 	current_user
 	end
-end
 
-def destroy
-	@check_list = Check_list.find(params[:id])
-	@check_list.destroy
-	redirect_to '/check_lists'
-end
+	def new
+		@check_list = Check_list.new
+		@colleges = College.all
+		current_user
+	end
+
+	def create
+		@check_list = Check_list.new(check_list_params)
+
+		if @check_list.save
+			redirect_to @check_list
+		end
+	end
+
+	def edit
+		@check_list = Check_list.find(params[:id])
+	end
+
+	def update
+		@check_list = Check_list.find(params[:id])
+
+		if @check_list.update(check_list_params)
+			redirect_to @check_list
+		end
+	end
+
+	def destroy
+		@check_list = Check_list.find(params[:id])
+		@check_list.destroy
+		redirect_to '/check_lists'
+	end
 
 private
 
-def check_list_params
-	params.require(:check_list).permit(:deadline, :transcript, :sat, :act, :application, :tour_date, :early_acceptance, :ranking, :rec_letters)
-end
+	def check_list_params
+		params.require(:check_list).permit(:deadline, :transcript, :sat, :act, :application, :tour_date, :early_acceptance, :ranking, :rec_letters)
+	end
+
+private
+
+  def require_login
+    unless current_user
+      redirect_to root_url, notice: 'You need to be logged in to post!'
+  	end
+  end
 
 end
