6ed9250b590c6370cc14a9874152c735f0e9e270
Lauren Grant <dangerlauren@gmail.com>
2015-09-30 15:17:55 -0500

    working on testing password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,13 @@ class User < ActiveRecord::Base
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes["password"] }
   validates :password, confirmation: true, if: -> { new_record? || changes["password"] }
+  
   validates :password_confirmation, presence: true, if: -> { new_record? || changes["password"] }
 
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, uniqueness: true
 
+  validates_format_of :first_name, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/
+ 
+  validates_format_of :last_name, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/
+  
 end
