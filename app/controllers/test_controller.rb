af000e2f5b018e097f3fb1d3a7aa7b9ddace0dcf
Robert Weilbaecher <robertweilbaecher@hotmail.com>
2015-10-01 12:05:53 -0500

    Fixed edge cases in college, and reseeded database

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -5,8 +5,31 @@ class TestController < ApplicationController
         @check_list = CheckList.new
         current_user
     end
+
+    def create
+        @check_list = CheckList.new(check_list_params)
+
+        if @check_list.save
+            redirect_to @check_list
+        end
+    end
+
     def whatup
         @college = College.find_by(name: params[:name])
+    end
+
+    private
+
+        def check_list_params
+            params.require(:check_list).permit(:deadline, :transcript, :essay, :sat, :act, :application, :tour_date, :early_acceptance, :ranking, :rec_letters, :user_id, :college_id)
+    end
+
+    private
+
+        def require_login
+            unless current_user
+                redirect_to root_url, notice: 'You need to be logged in to post!'
+            end
     end
     # private 
     #     def college_params
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,18 @@
+CheckList.destroy_all
 College.destroy_all
+User.destroy_all
 
 colleges = College.create([{name: "University of Texas at Austin", city: "Austin", state: "Texas", contact_info: "Sandra Lee 512-777-7890", address: "123 Main Street", tuition: 33842, financial_aid: true, scholarship: true},
 							{name: "Baylor University", city: "Waco", state: "Texas", contact_info: "Bob Danger 254-777-7890", address: "123 Main Street", tuition: 34480, financial_aid: true, scholarship: false},
-							{name: "Texas A&M", city: "College Station", state: "Texas", contact_info: "Connor O'Danger 123-456-7890", address: "123 Main Street", tuition: 26356, financial_aid: false, scholarship: false},
-							{name: "Texas State", city: "San Marcos", state: "Texas", contact_info: "Kyle T-Man Danger 234-567-8901", address: "123 Main Street", tuition: 31808, financial_aid: true, scholarship: false},
+							{name: "Texas A&M University", city: "College Station", state: "Texas", contact_info: "Connor O'Danger 123-456-7890", address: "123 Main Street", tuition: 26356, financial_aid: false, scholarship: false},
+							{name: "Texas State University", city: "San Marcos", state: "Texas", contact_info: "Kyle T-Man Danger 234-567-8901", address: "123 Main Street", tuition: 31808, financial_aid: true, scholarship: false},
 							{name: "Southern Methodist University", city: "Dallas", state: "Texas", contact_info: "Lauren Danger 345-678-9123", address: "123 Main Street", tuition: 41820, financial_aid: true, scholarship: true},
 							{name: "Rice University", city: "Houston", state: "Texas", contact_info: "Bruce Wayne 456-789-0123", address: "123 Main Street", tuition: 38941, financial_aid: true, scholarship: false},
 							{name: "Trinity University", city: "San Antonio", state: "Texas", contact_info: "Clark Kent 567-890-1234", address: "123 Main Street", tuition: 36214, financial_aid: false, scholarship: true},
 							{name: "University of North Texas", city: "Denton", state: "Texas", contact_info: "Bill Murray 678-901-2345", address: "123 Main Street", tuition: 26794, financial_aid: true, scholarship: true},
 							{name: "St. Edwards University", city: "Austin", state: "Texas", contact_info: "Bruce Willis 901-234-5678", address: "123 Main Street", tuition: 53200, financial_aid: true, scholarship: false},
-							{name: "University of Texas at San Antonio", city: "San Antonio", state: "Texas", contact_info: "Elvis Presley 888-987-5432", address: "123 Main Street", tuition: 21835, financial_aid: true, scholarship: true}])
+							{name: "University of Texas at San Antonio", city: "San Antonio", state: "Texas", contact_info: "Elvis Presley 888-987-5432", address: "123 Main Street", tuition: 21835, financial_aid: true, scholarship: true},
+							{name: "Danger State University", city: "Dangerville", state: "Texas", contact_info: "Mystical 326-437-9663", address: "66 Peligroso Avenue", tuition: 500, financial_aid: false, scholarship: false}])
 
 
 
